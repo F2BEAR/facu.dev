a4ff535e142e7dc4239412161170cdd47831f23a
F2BEAR <facucarbonel@hotmail.com>
2020-10-07 22:36:17 -0300

    first build

---
+++ client/build/precache-manifest.2400dd12c301177c986d3a610f2e59c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc62074ed22ebe5a6162055903921c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e4163fcd54c7eb9401f",
+    "url": "/static/css/main.1bda020f.chunk.css"
+  },
+  {
+    "revision": "379671c5c9a6e82e95da",
+    "url": "/static/js/2.c614e6c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c614e6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4163fcd54c7eb9401f",
+    "url": "/static/js/main.f72fbb58.chunk.js"
+  },
+  {
+    "revision": "fc977d5520a478d689ac",
+    "url": "/static/js/runtime-main.73250789.js"
+  }
+]);
---
+++ client/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+//const fs = require("fs");
+//const https = require("https");
+
+// const privateKey = fs.readFileSync("sslcert/privkey.pem", "utf8");
+// const certificate = fs.readFileSync("sslcert/cert.pem", "utf8");
+// const credentials = {key: privateKey, cert: certificate};
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// const httpsServer = https.createServer(credentials, app);
+
+const port = 420;
+
+// httpsServer.listen(port, () => {
+//     console.log(`Express escuchando en el puerto ${port}`);
+//   })
+
+app.listen(port, () => {
+  console.log(`Express escuchando en el puerto ${port}`);
+});
