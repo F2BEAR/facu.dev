998fbc43fac48166f158f7f831d0806ffb617bb5
Facundo Carbonel <facucarbonel@hotmail.com>
2026-03-26 14:54:27 -0300

    fix: upsis where made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,10 @@ import Resumee from "../components/Resumee";
 import Showcase from "../components/Showcase";
 import Header from "../components/Header";
 import Contact from "../components/Contact";
-import Nav from "../components/Nav";
 
 export default function Page() {
   return (
     <>
-      <Nav />
       <div className="flex min-h-screen flex-col items-center justify-center hero">
         <Header />
         <KeyInfo />
